f17fe325b2359c9409e15213208bd88526d9035b
Gwon YoungGwon <youngkwon@croquis.com>
2020-07-28 22:47:11 +0900

    10.5 - 4

--- 10.5.js
+++ 10.5.js
@@ -9,33 +9,39 @@ class Customer {
   get name() {}
   get billingPlan() {}
   get paymentHistory() {}
-  get isUnknow() { return false};
+  get isUnknow() {
+    return false;
+  }
 }
 
 class UnkownCustomer {
-  get isUnknow() { return false};
+  get isUnknow() {
+    return false;
+  }
 }
 
-function isUnkown(arg){
-  if(!((arg instanceof Customer) || (arg === "미확인 고객")))
+function isUnkown(arg) {
+  if (!(arg instanceof Customer || arg === "미확인 고객"))
     throw new Error(`잘못된 값과 비교 <${arg}>`);
-  
-  return (arg === "미확인 고객")
+
+  return arg === "미확인 고객";
 }
 
 // 1
-const aCustomer = site.customer;
+const aCustomer = new Site().customer;
 //.. 수 많은 코드
-let customerName;
-if (aCustomer === "미확인 고객") customerName = "거주자";
+let customerName = "";
+if (isUnkown(aCustomer)) customerName = "거주자";
 else customerName = aCustomer.name;
 
 // 2
-const plan = (aCustomer === "미확인 고객") ?
- registry.billingPlan.basic : aCustomer.billingPlan;
+const plan = isUnkown(aCustomer)
+  ? registry.billingPlan.basic
+  : aCustomer.billingPlan;
 
 // 3
-if(aCustomer !=== "미확인 고객") aCustomer.billingPlan = newPlan;
+if (!isUnkown(aCustomer)) aCustomer.billingPlan = newPlan;
 
-const weeksDelinquent = (aCustomer === "미확인고객")?
-0 : aCustomer.paymentHistory.weeksDelinquent;
+const weeksDelinquent = isUnkown(aCustomer)
+  ? 0
+  : aCustomer.paymentHistory.weeksDelinquent;
